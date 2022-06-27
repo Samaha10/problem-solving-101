1cd34804f91310bdfc848dfd8666f715c15b8916
Samaha10 <56085746+Samaha10@users.noreply.github.com>
2022-06-27 03:06:03 +0200

    Update 49. Group Anagrams.py

--- 49. Group Anagrams.py
+++ 49. Group Anagrams.py
@@ -8,8 +8,7 @@ class Solution:
             
             for letter in word:
                 freq[letter] = 1 + freq.get(letter, 0)
-            
-            # tuple(sorted(freq.items()))
+           
             hashgroup[frozenset(freq.items())].append(word)
         
         return hashgroup.values()
