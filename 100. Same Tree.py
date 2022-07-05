7ebfca90f931fa684ca46869e270814f00ff2889
Samaha10 <56085746+Samaha10@users.noreply.github.com>
2022-07-05 04:15:48 +0200

    Add files via upload

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -11,5 +11,5 @@ class Solution:
         elif not p or not q:
             return False
         
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p.val == q.val and self.isSameTree(p.left, q.left) and                         self.isSameTree(p.right, q.right)
         
