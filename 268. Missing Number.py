0abf81d92dfefb8c2bfc642a68f41defd7f9668b
Samaha10 <56085746+Samaha10@users.noreply.github.com>
2022-07-06 17:07:02 +0200

    Add files via upload

---
+++ 268. Missing Number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # if 0 not in nums return 0
+        passed = [-1] * (len(nums) + 1)
+        
+        for elem in nums:
+            passed[elem] = 1
+        
+        for i, elem in enumerate(passed):
+            if elem == -1:
+                return i
+        
+        
+        return len(passed)
