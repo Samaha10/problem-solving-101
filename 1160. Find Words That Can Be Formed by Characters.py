294f197113f5548778b74ac803debde8c6444f39
Samaha10 <56085746+Samaha10@users.noreply.github.com>
2022-07-27 14:50:38 +0200

    Add files via upload

---
+++ 1160. Find Words That Can Be Formed by Characters.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        guide = dict()
+        total = 0
+        
+        for lett in chars:
+            guide[lett] = 1 + guide.get(lett, 0)
+        
+        
+        for word in words:
+            w_count = dict()
+            
+            for lett in word:
+                w_count[lett] = 1 + w_count.get(lett, 0)
+            
+            
+            for l in w_count:
+                if w_count[l] > guide.get(l, 0):
+                    break
+            else:
+                total += len(word)
+            
+
+            
+        
+        return total
